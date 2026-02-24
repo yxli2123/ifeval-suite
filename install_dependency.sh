94432f610ad4d1f9f93ce67ce51a078833c4a7bb
Yixiao Li <yxli2123@gmail.com>
2026-02-24 12:32:29 -0500

    Add dependency installer.

---
+++ install_dependency.sh
@@ -0,0 +1,11 @@
+pip install nltk spacy openai
+pip install absl-py langdetect immutabledict emoji syllapy
+pip install unicodedata
+pip install "lm_eval[hf]"
+pip install -U "transformers>=4.56,<5"
+pip install google.generativeai mistralai anthropic pythainlp
+pip install boto3
+
+python -m nltk.downloader punkt
+python -m nltk.downloader punkt_tab
+python -m spacy download en_core_web_sm
